aa67a1861306832af8fe3f1f499c97d2e3b13194
Randi ATEWOUNG <randi-vairel.atewoung-sonfack@etu.u-pec.fr>
2024-03-01 12:41:26 +0100

    Adding script and log files

---
+++ analyse.bat
@@ -0,0 +1,11 @@
+echo "Extractions des valeurs d'analyse sur le Raspberry pi 2"
+
+ssh -p 24 -t toto@pi-2 tail -n 250 /var/log/rpi.log > extractions.log
+
+ssh -t toto@dd-8.inside.esipe-creteil.info tail -n 500 /var/log/rpi.log > stress.log
+
+echo "Les données ont été extraites nous allons tracé le graphique correspondant"
+
+python analyse.py log=extractions.log
+
+echo "Tracé du graphique réussi"
---
+++ analyse.py
@@ -0,0 +1,95 @@
+import sys
+
+import matplotlib.pyplot as plt
+
+
+def analyseEnergy(log, stress):
+    # Ouvrir le fichier en lecture seule
+    logFile = open(log, "r")
+    stressFile = open(stress, "r")
+
+    # utiliser readlines pour lire toutes les lignes du fichier
+    # La variable "lignes" est une liste contenant toutes les lignes du fichier
+    logLines = logFile.readlines()
+    stressLines = stressFile.readlines()
+
+    # fermez le fichier après avoir lu les lignes
+    logFile.close()
+    stressFile.close()
+
+    # Tableaux pour le tracé du graphe intensité=f(temps)
+    logTimes = []
+    logIntensities = []
+
+    stressTimes = []
+    stressIntensities = []
+
+    # Iterer sur les lignes
+    for line in logLines:
+        if line != "\n":
+            result_of_extraction = line.split(" ")
+
+            # Recuperation de la date
+            time = result_of_extraction[1]
+            logTimes.append(time)
+
+            # Recuperation de l'intensite
+            intensity = result_of_extraction[2]
+            logIntensities.append(float(intensity))
+
+    for line in stressLines:
+        if line != "\n":
+            result_of_extraction = line.split(",")
+
+            # Recuperation de la date
+            date = result_of_extraction[0]
+            date_split = date.split(" ")
+            time = date_split[1]
+            stressTimes.append(time)
+
+            # Recuperation de l'intensite
+            intensity = 0
+            for i in range(1, 4):
+                intensity += float(result_of_extraction[i])
+            stressIntensities.append(intensity)
+
+    logMoy = sum(logIntensities) / len(logIntensities)
+    stressMoy = sum(stressIntensities) / len(stressIntensities)
+
+    fig, axs = plt.subplots(2, 1, figsize=(30, 7))
+
+    axs[0].plot(logTimes, logIntensities, color='tab:blue', label="Intensité relevées lors de la MàJ")
+    axs[0].set_xlabel('temps')
+    axs[0].set_ylabel('Intensité')
+    axs[0].set_title(f'Intensité relevées sur le Raspberry Pi en fonction du temps\nmoyenne = {logMoy}')
+
+    axs[1].plot(stressTimes, stressIntensities, color='tab:red',
+                label=f"Intensité relevées lors du stress de la machine\nmoyenne = {stressMoy}")
+    axs[1].set_xlabel('temps')
+    axs[1].set_ylabel('Intensité')
+    axs[1].set_title('Intensité relevées lors du stress de la machine en fonction du temps')
+
+    axs[0].set_xticks(logTimes[::10])
+    axs[0].set_xticklabels(logTimes[::10], rotation=60)
+    axs[1].set_xticks(stressTimes[::10])
+    axs[0].set_xticklabels(stressTimes[::10], rotation=60)
+
+    plt.tight_layout()
+
+    plt.legend()
+    plt.show()
+
+    return None
+
+
+if __name__ == '__main__':
+    FICHIER_DE_LOG = "rpi.log"
+    FICHIER_DE_MACHINE_STRESS = "stress.log"
+    for strParam in sys.argv:
+        param = strParam.split("=")
+        if param[0] == "log":
+            FICHIER_DE_LOG = param[1]
+        elif param[0] == "stress":
+            FICHIER_DE_MACHINE_STRESS = param[1]
+
+    analyseEnergy(FICHIER_DE_LOG, FICHIER_DE_MACHINE_STRESS)
